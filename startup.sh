5d616dc71e736c2e72c3ff7ced495fbf31162f19
Ruben Vandamme <github-com-00ff86@vandamme.email>
2020-06-01 11:13:39 +0200

    Run pulseaudio-dlna in the foreground.

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@ set -x
 dbus-daemon --system --fork
 pulseaudio --log-level=1 --system &
 /librespot-e8ec5feb2260e9d2e3b8bfc927cc5211d626e015/target/release/librespot -n "${LIBRESPOT_NAME:-DLNASpotify}" -b 320 --disable-audio-cache --initial-volume 100 --device-type speaker ${LIBRESPOT_OPTS}&
-pulseaudio-dlna --debug &
-sleep 24h
+sleep 1
+pulseaudio-dlna --debug
