4113e1d9bb3a273920dbbf4a9afc01ec5a0e6953
Ruben Vandamme <github-com-00ff86@vandamme.email>
2020-06-07 21:45:43 +0200

    Quote PULSEAUDIO_DLNA_OPTS

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ dbus-daemon --system --fork
 pulseaudio --log-level=1 --system &
 /librespot-e8ec5feb2260e9d2e3b8bfc927cc5211d626e015/target/release/librespot -n "${LIBRESPOT_NAME:-DLNASpotify}" -b 320 --disable-audio-cache --initial-volume 100 --device-type speaker ${LIBRESPOT_OPTS}&
 sleep 1
-pulseaudio-dlna --debug ${PULSEAUDIO_DLNA_OPTS}
+pulseaudio-dlna --debug "${PULSEAUDIO_DLNA_OPTS}"
