5ed4a7a61341bc8ebb527d0663bdb00420dbac29
vx3r <vx3r@127-0-0-1.fr>
2021-05-12 19:25:42 +0200

    update librespot, minor changes

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 set -x
 dbus-daemon --system --fork
 pulseaudio --log-level=1 --system &
-/librespot-e8ec5feb2260e9d2e3b8bfc927cc5211d626e015/target/release/librespot -n "${LIBRESPOT_NAME:-DLNASpotify}" -b 320 --disable-audio-cache --initial-volume 100 --device-type speaker ${LIBRESPOT_OPTS}&
+/librespot-0.2.0/target/release/librespot -n "${LIBRESPOT_NAME:-DLNASpotify}" -b 320 --disable-audio-cache --initial-volume 100 --device-type speaker ${LIBRESPOT_OPTS}&
 sleep 1
-pulseaudio-dlna --debug "${PULSEAUDIO_DLNA_OPTS}"
+pulseaudio-dlna ${PULSEAUDIO_DLNA_OPTS}
